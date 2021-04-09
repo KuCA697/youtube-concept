55da1296080667f775dd1145842429c2ea4ee8ae
KuCA697 <81879273+KuCA697@users.noreply.github.com>
2021-04-09 21:39:47 +0300

    Добавили правила для адаптива

--- js/main.js
+++ js/main.js
@@ -1,11 +1,34 @@
-const swiper = new Swiper('.channel-slider', {
+const mychannel = new Swiper('.channel-slider', {
   // Optional parameters
   loop: true,
-  slidesPerView: 6,
-
+  slidesPerView: 1, //количество колонок в слайдере
+  spaceBetween: 20, //расстояние между картинками в слайдере
+  breakpoints: { //поведение слайдера в разных точках
+    1900: { //для экранов с разрешением больше 1900
+      slidesPerView: 6, //количество колонок в слайдере
+    },
+    1600:{ 
+      slidesPerView: 5, //количество колонок в слайдере
+    },
+    1300:{ 
+      slidesPerView: 4,
+    },
+    1100:{ 
+      slidesPerView: 3,
+    },
+    800:{ 
+      slidesPerView: 2,
+    }
+  },
   // Navigation arrows
   navigation: {
-    nextEl: '.channel-button-next',
-    prevEl: '.channel-button-prev',
+    nextEl: 'channel-button-next',
+    prevEl: 'channel-button-prev',
   },
+});
+
+const searchBth = document.querySelector('.mobile-search');
+const mobileSearch = document.querySelector('.input-group');
+searchBth.addEventListener('click',() => {
+ mobileSearch.classList.toggle('is-open');
 });
